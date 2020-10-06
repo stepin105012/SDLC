94c5bd88852c017ff2a29a91271a44886abedf3d
Binitta Michael <69056129+stepin105012@users.noreply.github.com>
2020-10-06 22:47:33 +0530

    Update numbers.h

--- 3_Implementation/inc/numbers.h
+++ 3_Implementation/inc/numbers.h
@@ -8,11 +8,5 @@ int armstrong(int number);
 int even_odd(int number);
 int palindrome(int number);
 int power(int number);
-int harshad(int number);
-int perfect_square(int number);
-int perfect_cube(int number);
-int automorphic_number(int number);
-int divisibility_3(int number);
-int divisibility_5(int number);
-int divisibility_7(int number);
+
 #endif
